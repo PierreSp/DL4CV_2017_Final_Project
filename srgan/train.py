13f355b528c36d577cc90eb63c88feddbb0e7133
Pierre <pierre.springer@tum.de>
2018-01-17 21:55:06 +0100

    minor bugfix

--- srgan/train.py
+++ srgan/train.py
@@ -50,7 +50,7 @@ def parse_args():
         '--no-cuda', action='store_true',
         help='override cuda and use cpu, even if cuda is available')
     parser.add_argument(
-        '--network', default="vgg19", type=int, help='Options: "vgg16" and "vgg19"')
+        '--network', default="vgg19", type=str, help='Options: "vgg16" and "vgg19"')
     parser.add_argument(
         '--weight_perception', default=0.006, type=float, help='define the loss multiplicator for the perception')
     return parser.parse_args()
