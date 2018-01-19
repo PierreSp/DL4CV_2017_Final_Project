06b000bcc91319261b83e85d1126011a691ff6f5
Nathanael Bosch <nathanael.bosch@tum.de>
2018-01-19 16:14:26 +0100

    added parameters to dummy model

--- srgan/dummy_model.py
+++ srgan/dummy_model.py
@@ -4,6 +4,7 @@ import torch
 class DummyModel(torch.nn.Module):
     def __init__(self):
         super(DummyModel, self).__init__()
+        self.conv = torch.nn.Conv2D(1, 1, 1)
 
     def forward(self, x):
         return torch.autograd.Variable(torch.Tensor(1))
