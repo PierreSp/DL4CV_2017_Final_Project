97570d758d974162ad8da3e967d99d123a25ac23
Nathanael Bosch <nathanael.bosch@tum.de>
2018-01-19 14:57:15 +0100

    bash script to run the different variants we wanted to test

---
+++ run_comparisons.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+# 1. Test:
+python -m srgan.train --num_epochs 1 --batch_size 512
+
+# 2. ONLY specific loss
+# 2.1. only perception
+python -m srgan.train --batch_size 512 --no-discriminator --weight_image 0
+# 2.2. only pixel-wise
+python -m srgan.train --batch_size 512 --no-discriminator --weight_perception 0
+# 2.3. only adversarial
+python -m srgan.train --batch_size 512 --weight_image 0 --weight_perception 0
+
+# 2. 2/3 losses
+# 2.1. no pixel-wise
+python -m srgan.train --batch_size 512 --no-discriminator --weight_image 0
+# 2.2. no discriminator
+python -m srgan.train --batch_size 512 --no-discriminator
+# 2.3. no perception
+python -m srgan.train --batch_size 512 --weight_perception 0
+
+# 3. VGG16 vs VGG19 vs VGG16&VGG19
+python -m srgan.train --batch_size 512 --network vgg16
+python -m srgan.train --batch_size 512 --network vgg19
+python -m srgan.train --batch_size 512 --network vgg16vgg19
+
